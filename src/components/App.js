b76282b1bee002ca6170986cd8048402478859ca
Andrew <andrewonulak@gmail.com>
2023-03-01 00:14:56 -0700

    Testing added route from db:

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
 function App() {
+  const [state, setState] = useState()
+
+  useEffect(() => {
+    fetch("http://localhost:9292/lego_sets")
+    .then(resp.json())
+    .then(console.log(d))
+  },[])
+
   return (
     <div className="App">
       <h1>testing</h1>
