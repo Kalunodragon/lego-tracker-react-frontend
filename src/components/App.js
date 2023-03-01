c7e8858c5a24128f99cc3558c0835224248da35b
Andrew <andrewonulak@gmail.com>
2023-03-01 02:55:15 -0700

    Starting build out of client side

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,11 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
+import Home from "./Home";
 
 function App() {
-  const [state, setState] = useState([])
-
-  useEffect(() => {
-    fetch("http://localhost:9292/lego_sets")
-    .then(r => r.json())
-    .then(d => {
-      console.log(d)
-      setState(d)
-    })
-  },[])
-
-  const display = state.map(set => {
-    return(
-      <div  key={set.name} className="Sets">
-        <h1>{set.name}</h1>
-        <p>Number of peices: {set.peices}</p>
-        <p>Set number: {set.set_number}</p>
-        <p>Theme: {set.theme}</p>
-      </div>
-    )
-  })
-
-  if (!state) {
-    return (
-      <h1>Loading</h1>
-    )
-  }
 
   return (
     <div className="App">
-      {display}
+      <Home />
     </div>
   );
 }
