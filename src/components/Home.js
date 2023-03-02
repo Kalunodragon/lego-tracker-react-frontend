9c0e05a41281519a875a41603a4ad83789a08fc0
Andrew <andrewonulak@gmail.com>
2023-03-02 11:45:11 -0700

    Adding initial text for Home page, and some style for it

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,12 @@ function Home() {
 
   return(
     <>
-      <h1>Home Test</h1>
+      <h1>Home</h1>
+      <p>Welcome to the LEGO Tracker application</p>
+      <p>This Tracker can be used to keep track of multiple LEGO sets owned by numerous owners. <br></br>
+      It will also allow the owner to make notes about their sets to keep tract of if they <br></br>
+      liked the set or not. Using the Navigation bar above you can select the page you want to <br></br>
+      go to and interact with adding new lego sets, or adding</p>
     </>
   )
 
