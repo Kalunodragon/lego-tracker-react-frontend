950b7dfabf2d9f4488303fdf060aaac406c433a3
Andrew <andrewonulak@gmail.com>
2023-03-02 15:16:58 -0700

    Working on getting the LEGO set form, set up, Need to get the mapping of unique themes to work

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import NavBar from "./NavBar";
@@ -6,6 +6,25 @@ import LegoSets from "./LegoSets"
 import Owners from "./Owners"
 
 function App() {
+  const [testing, setTesting] = useState(null)
+  const [themes, setThemes] = useState(null)
+  let allThemes
+
+  useEffect(() => {
+    fetch("http://localhost:9292/lego_sets")
+    .then(r => r.json())
+    .then(d => setTesting(d))
+  },[])
+
+  if(testing !== null) allThemes = (testing.map(set => set.theme.theme))
+  const uniqueThemes = [...new Set(allThemes)]
+  console.log(uniqueThemes)
+
+  // const allThemes = testing.map(theme => theme.theme.theme)
+  // const uniqueThemes = [...new Set(allThemes)]
+  // if(themes !== uniqueThemes) setThemes(uniqueThemes)
+
+  if(!testing) return <h1>LOADING...</h1>
 
   return (
     <div className="App">
@@ -15,7 +34,7 @@ function App() {
           <Home />
         </Route> 
         <Route exact path="/lego_sets">
-          <LegoSets />
+          <LegoSets themeOptions={uniqueThemes}/>
         </Route>
         <Route exact path="/owners">
           <Owners />
--- src/components/LegoSetForm.js
+++ src/components/LegoSetForm.js
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function LegoSetForm(){
+function LegoSetForm({ themeOptions }){
+  const [mappedOptions, setMappedOptions] = useState(themeOptions)
+  const [formData, setFormData] = useState({
+    "name": "",
+    "setNumber": "",
+    "pieces": "",
+    "theme": "",
+    "age": ""
+  })
+
+  useEffect(() => {
+    setMappedOptions([...new Set(mappedOptions)].map(opt => {
+      return <option value={opt}>{opt}</option>
+    }))
+  },[])
+
+  console.log("from Form: ", themeOptions)
+
+  function handleChange(e){
+    const keyName = (e.target.name)
+    const value = (e.target.value)
+    setFormData({
+      ...formData,
+      [keyName]: value
+    })
+  }
 
   return(
     <>
-      <h1>set form Testing</h1>
+      <h1>Add A New Lego Set</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Name of LEGO set"
+          name="name"
+          onChange={handleChange}>
+        </input>
+        <input
+          type="text"
+          placeholder="LEGO set number"
+          name="setNumber"
+          onChange={handleChange}>
+        </input>
+        <input
+          type="text"
+          placeholder="Number of LEGO pieces in set"
+          name="pieces"
+          onChange={handleChange}>
+        </input>
+        <select>
+          {mappedOptions}
+        </select>
+        <input
+          type="text"
+          placeholder="Recommended age"
+          name="age"
+          onChange={handleChange}>
+        </input>
+      </form>
     </>
   )
 }
