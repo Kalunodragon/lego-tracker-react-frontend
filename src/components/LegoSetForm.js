4161a5b9919391a864ddba47f83e59b36e5e18bc
Andrew <andrewonulak@gmail.com>
2023-03-02 22:28:02 -0700

    Finished setting up Controlled Form with a controlled dropdown that can add a new theme

--- src/components/LegoSetForm.js
+++ src/components/LegoSetForm.js
@@ -7,7 +7,8 @@ function LegoSetForm(){
     "setNumber": "",
     "pieces": "",
     "theme": "select",
-    "age": ""
+    "age": "",
+    "addTheme": ""
   })
 
   useEffect(() => {
@@ -27,45 +28,73 @@ function LegoSetForm(){
 
   function handleSubmit(e){
     e.preventDefault()
-    console.log(formData)
+    
+    let themeData = ""
+    if (formData.theme === "create"){
+      themeData = formData.addTheme
+    } else {
+      themeData = formData.theme
+    }
+
+    console.log({
+      "name": formData.name,
+      "setNumber": parseInt(formData.setNumber),
+      "pieces": parseInt(formData.pieces),
+      "theme": themeData,
+      "age": parseInt(formData.age)
+    })
   }
+
+  const addThemeInput = formData.theme === "create" ? 
+    <input
+      className="form-input"
+      type="text"
+      placeholder="Theme to add"
+      name="addTheme"
+      onChange={handleChange}>
+    </input> : null
 
   if(!themes) return <h1>LOADING...</h1>
 
   return(
     <>
       <h1>Add A New Lego Set</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="form-main">
         <input
+          className="form-input"
           type="text"
           placeholder="Name of LEGO set"
           name="name"
           onChange={handleChange}>
         </input>
         <input
+          className="form-input"
           type="text"
           placeholder="LEGO set number"
           name="setNumber"
           onChange={handleChange}>
         </input>
         <input
+          className="form-input"
           type="text"
           placeholder="Number of LEGO pieces in set"
           name="pieces"
           onChange={handleChange}>
         </input>
-        <select name="theme" onChange={handleChange}>
+        <select name="theme" onChange={handleChange} className="form-select">
           <option defaultValue="Select">Select an option</option>
           <option value="create">Create New Theme </option>
           {themes}
         </select>
+        {addThemeInput}
         <input
+          className="form-input"
           type="text"
           placeholder="Recommended age"
           name="age"
           onChange={handleChange}>
         </input>
-        <button>Submit</button>
+        <button className="form-submit">Submit</button>
       </form>
     </>
   )
